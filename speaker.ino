949d308fc84493f0aa1b485635687f371293eb3a
magatte-01 <65225206+magatte-01@users.noreply.github.com>
2021-01-11 15:34:00 +0100

    Update speaker.ino

--- speaker.ino
+++ speaker.ino
@@ -1,89 +1,52 @@
-int PIN_BUZZER = 9;
+//www.idehack.com
+#include <SPI.h>
+#include <SD.h> // Inclure la librairie SD
+#define SDPIN 10 // Chip Select du lecteur SD
+#include <TMRpcm.h> 
+
+File myFile;
+TMRpcm tmrpcm;
+const int speakerPin=9;//47
+char* CroquetteSound = (char*)"sounds/hey.WAV";
+char caractere; // Définition du caractère pour la gestion dans le moniteur série
 
 void setup(){
-  Serial.begin (115200);
+  Serial.begin(9600); //début de la communication série
 
-  pinMode(PIN_BUZZER, OUTPUT);
+   // Initialisation de la carte SD
+  if (!SD.begin(SDPIN)) { 
+    Serial.println("initialization failed!"); //  S'il y a un soucis "initialization failed!" s'affichera au moniteur
+    return;
+  }
+
+  //Init speaker
+  tmrpcm.speakerPin = speakerPin; // Speaker sur la pin 9
+  tmrpcm.setVolume(7); // gestion du volume de 0 à 7
+  tmrpcm.quality(1); // qualitée audio 0 ou 1
+  
+  Serial.println("tapez un caractere -> a");
 }
 
 void loop(){
-  
-  tone(PIN_BUZZER, 2637, 200);
-  delay(400);
-  tone(PIN_BUZZER, 1975, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2093, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2349, 200);
-  delay(400);
-  tone(PIN_BUZZER, 2093, 200);
-  delay(200);
-  tone(PIN_BUZZER, 1975, 200);
-  delay(200);
-  tone(PIN_BUZZER, 1760, 200);
-  delay(400);
-  tone(PIN_BUZZER, 1760, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2093, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2637, 200);
-  delay(400);
-  tone(PIN_BUZZER, 2349, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2093, 200);
-  delay(200);
-  tone(PIN_BUZZER, 1975, 200);
-  delay(400);
-  tone(PIN_BUZZER, 1975, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2093, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2349, 200);
-  delay(400);
-  tone(PIN_BUZZER, 2637, 200);
-  delay(400);
-  tone(PIN_BUZZER, 2093, 200);
-  delay(400);
-  tone(PIN_BUZZER, 1760, 200);
-  delay(400);
-  tone(PIN_BUZZER, 1760, 200);
-  delay(800);
-  tone(PIN_BUZZER, 1760, 200);
-  delay(400);
-  tone(PIN_BUZZER, 2349, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2794, 200);
-  delay(200);
-  tone(PIN_BUZZER, 3520, 200);
-  delay(400);
-  tone(PIN_BUZZER, 3136, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2794, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2637, 200);
-  delay(600);
-  tone(PIN_BUZZER, 2093, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2637, 200);
-  delay(400);
-  tone(PIN_BUZZER, 2349, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2093, 200);
-  delay(200);
-  tone(PIN_BUZZER, 1975, 200);
-  delay(400);
-  tone(PIN_BUZZER, 1975, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2093, 200);
-  delay(200);
-  tone(PIN_BUZZER, 2349, 200);
-  delay(400);
-  tone(PIN_BUZZER, 2637, 200);
-  delay(400);
-  tone(PIN_BUZZER, 2093, 200);
-  delay(400);
-  tone(PIN_BUZZER, 1760, 200);
-  delay(400);
-  tone(PIN_BUZZER, 1760, 200);
-  delay(800);
+    if(Serial.available()){
+      caractere = Serial.read();
+      if(caractere == 'a') { // Si le caractère recu dans le moniteur série est "a"
+        Serial.println("Lecture"); // Ecrit dans le moniteur série 
+        tmrpcm.play(CroquetteSound); // Joue le son "son8bit.wav"
+          if(tmrpcm.isPlaying())
+          {
+            Serial.println("Fichier en cours de lecture");
+          }
+          else
+          {
+            Serial.println("Fichier pas lu");
+          }
+      }
+    }
+}
+ 
+void playSound(char *file)
+{
+ tmrpcm.play(file);
+ return; 
 }
